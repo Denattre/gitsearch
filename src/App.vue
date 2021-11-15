1b9b641dfcb5d804cbe89fddc29300325ee181fb
Dmitry Ivanov <silen919@gmail.com>
2021-11-15 22:53:00 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+   <search @input="debouncedSearch" v-model="query" />
+   <ul>
+     <li v-for="rep in repList" :key="rep.name">
+       <h2>Заголовок разделитель</h2>
+       <ul>
+      <li>
+        <!-- <a href="${rep.owner.html_url}">{{rep.owner.login}}</a> -->
+      </li>
+      <li>
+        <!-- <a href="${rep.html_url}">{{rep.name}}</a> -->
+      </li>
+      <!-- <li>{{rep.language}}</li>
+      <li>{{rep.stargazers_count}}</li> -->
+   </ul>
+     </li>
+   </ul>
+   
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Search from './components/Search.vue'
+import _debounce from 'lodash.debounce';
+import axios from 'axios'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Search,
+  },
+  data() {
+    return {
+      query: '',
+      repList: null,
+      }
+  },
+  methods: {
+    debouncedSearch: _debounce(function () {
+      axios.get(`https://api.github.com/search/repositories?q=${this.query}`)
+      .then(function (response) {
+      this.repList = response.data.items
+      console.log(this.repList)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      }, 500),
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input :value="modelValue" @input="updateInput" type="text" placeholder="Введите текст">
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props:{
+      modelValue:[String],
+    },
+    methods: {
+      updateInput(event){
+        this.$emit('update:modelValue', event.target.value)
+      }
+      
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
